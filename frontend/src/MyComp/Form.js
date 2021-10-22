1ecaaa4bd40775950f6b1be626f2117b80b83181
Sunnypatil002 <sunnypatil4006@gmail.com>
2021-10-22 17:32:32 +0530

    database routing

--- frontend/src/MyComp/Form.js
+++ frontend/src/MyComp/Form.js
@@ -16,10 +16,39 @@ export default function Form() {
         setUser({ ...user, [a]:value});
     }  
 
+    // const PostData =async  (e)=>{
+    //         e.preventDefault();
+    //         const { name,email,phone} = user;
+
+    //         const res = await fetch("/register",{
+    //             method: "POST",
+    //             headers: {
+    //             "COntent-Type": "application/json"
+    //         },
+    //         body:JSON.stringify({
+    //             name ,email ,phone
+    //         })
+    //         });
+
+    //         const data = await res.json();
+
+    //         if(data.status=== 422 || !data){
+    //             window.alert("invalid Registration");
+    //             console.log("invalid Registration");
+    //         }
+    //         else {
+    //             window.alert(" Registration SUccessful");
+    //             console.log(" Registration SUccessful");
+                
+    //         }
+
+
+    // }
+
     return (
         <div>
             <Navbar/>
-            <div className='container'>
+            <div method="POST" className='container'>
                 <h3>REGISTRATION FORM</h3>
                 <div className='form-first-div'>
                 <div className="mb-3">
@@ -34,7 +63,8 @@ export default function Form() {
                     <label for="exampleFormControlInput1" className="form-label">Contact no.</label>
                     <input type="number" name="phone"className="form-control"value={user.phone}onChange={handleInputs}  id="exampleFormControlInput1" placeholder="9999999999" />
                 </div>
-                <button className='btn btn-primary'>Submit</button>
+                <button className='btn btn-primary' >Submit</button>
+                
 
                 </div>
             </div>
